c0b0e3c024e8869648ed3ebca72045c60fdebdea
Alfredo <aguirre.alfredo07@gmail.com>
2010-02-19 11:04:31 +0000

    Stop the cursor from blinking

--- emacs.d/config-color-themes.el
+++ emacs.d/config-color-themes.el
@@ -5,3 +5,7 @@
 (ansi-color-for-comint-mode-on)
 (setq inhibit-startup-message t)
 (set-default 'indicate-empty-lines nil)
+(require 'cursor-chg)
+(change-cursor-mode 1)
+(change-cursor-mode 1) ; On for overwrite/read-only/input mode
+(toggle-cursor-type-when-idle 1) ; On when idle
