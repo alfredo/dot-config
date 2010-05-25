252a192fae5162474c6644d41dc45ad3430e4584
Alfredo <aguirre.alfredo07@gmail.com>
2010-05-25 22:24:50 +0100

    More python stuff

--- emacs.d/config-python-mode.el
+++ emacs.d/config-python-mode.el
@@ -12,23 +12,13 @@
 
 (load-library "flymake-cursor")
 
-
-;;; Electric Pairs
-;(add-hook 'python-mode-hook
-;     (lambda ()
-;      (define-key python-mode-map "(" 'electric-pair)
-;      (define-key python-mode-map "[" 'electric-pair)
-;      (define-key python-mode-map "{" 'electric-pair)))
-;(defun electric-pair ()
-;  "Insert character pair without sournding spaces"
-;  (interactive)
-;  (let (parens-require-spaces)
-;    (insert-pair)))
-
+(require 'ipython)
+(setq python-python-command "ipython")
+(setq py-python-command-args '( "-colors" "Linux"))
 
 ; bind RET to py-newline-and-indent
-(add-hook 'python-mode-hook '(lambda () 
-     (define-key python-mode-map "\C-m" 'newline-and-indent)))
+;(add-hook 'python-mode-hook '(lambda ()
+;     (define-key python-mode-map "\C-m" 'newline-and-indent)))
 
 
 ;; virtualenv loading
