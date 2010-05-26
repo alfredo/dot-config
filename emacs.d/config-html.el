48c976e270b952e8dc5a8ce0c878b70657a75472
Alfredo <aguirre.alfredo07@gmail.com>
2010-05-26 21:09:55 +0100

    Sass support

--- emacs.d/config-html.el
+++ emacs.d/config-html.el
@@ -2,13 +2,14 @@
 (setq auto-mode-alist (cons '("\\.html$" . html-mode) auto-mode-alist))
 
 (add-to-list 'auto-mode-alist '("\\.less\\'" . css-mode))
-(add-to-list 'auto-mode-alist '("\\.sass\\'" . css-mode))
+;;(add-to-list 'auto-mode-alist '("\\.sass\\'" . css-mode))
 
-(load "lorem-ipsum")
-
-(defun iwb ()
+(defun fix-buffer ()
   "indent whole buffer"
   (interactive)
   (delete-trailing-whitespace)
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max)))
+
+(require 'sass-mode)
+(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
