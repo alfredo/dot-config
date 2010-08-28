002d41e8caadc84a0450aa04c81ec56b3ac9a7e8
Alfredo <aguirre.alfredo07@gmail.com>
2010-08-28 22:46:33 +0100

    Grails mode

--- emacs.d/config-groovy.el
+++ emacs.d/config-groovy.el
@@ -1,7 +1,6 @@
-;;; turn on syntax hilighting
-(global-font-lock-mode 1)
-
 ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
 (autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+
